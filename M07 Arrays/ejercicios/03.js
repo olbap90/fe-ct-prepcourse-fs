de8fc59fd29deed1345eaf52fd5d8a0b64bb743d
Pablo <olbap_olimpo@hotmail.com>
2024-02-15 18:13:57 -0300

    ahora

---
+++ M07 Arrays/ejercicios/03.js
@@ -0,0 +1,9 @@
+function encontrarElemento(elemento, array) {
+  // Busca el elemento pasado por argumento dentro del array.
+  // Si no se encuentra, retorna -1.
+  // Tu código:
+  var indice = array.indexOf(elemento);
+  return indice;
+}
+
+module.exports = encontrarElemento;
