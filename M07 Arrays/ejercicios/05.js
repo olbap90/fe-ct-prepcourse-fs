2156976d3359bce3635541ffc5bb3ebc418d74d8
celeste <olbap_olimpo@hotmail.com>
2024-02-15 17:05:22 -0300

    ahora

--- M07 Arrays/ejercicios/05.js
+++ M07 Arrays/ejercicios/05.js
@@ -1,6 +1,15 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string' && array[i].length > 5) {
+      return array[i]
+    }
+  }
+  return null
 }
+var array = [1, 'abc', 'defgh', 2, 'ijklm', 'nop'];
+var primerStringLargo = obtenerPrimerStringLargo(array);
+console.log(primerStringLargo); // Imprimirá 'defgh'
 
 module.exports = obtenerPrimerStringLargo;
--- M07 Arrays/ejercicios/06.js
+++ M07 Arrays/ejercicios/06.js
@@ -2,6 +2,10 @@ function duplicarElementos(array) {
   // Duplica (multiplica x2) cada elemento del array de números.
   // Devuelve un array con los duplicados.
   // Tu código:
+  var arrayDuplicado = array.map(function(elemento) {
+    return elemento * 2;
+  });
+  return arrayDuplicado;
 }
 
 module.exports = duplicarElementos;
