b8ee8b53b0d8d45d4208bf148c4ee3a6b13d21f4
Pablo <olbap_olimpo@hotmail.com>
2024-02-21 20:54:55 -0300

    21-2

--- C7 Arrays/ejercicios/07 2.js
+++ C7 Arrays/ejercicios/07 2.js
@@ -2,6 +2,10 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  array.sort(function(a,b){
+    return a - b;
+  });
+  return array;
 }
 
 module.exports = ordenarArray;
